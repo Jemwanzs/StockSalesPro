52710ff569e5286778821a6bcdb9d8844ecb4b5d
Jemwanzs <jamosammy@gmail.com>
2025-05-31 18:30:38 +0300

    Code protect

--- src/main.tsx
+++ src/main.tsx
@@ -5,3 +5,14 @@ import './index.css'
 
 // Remove dark mode class addition
 createRoot(document.getElementById("root")!).render(<App />);
+
+//--code copy protection < right click and other chortcuts dissabled-- >
+document.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+});
+
+document.addEventListener('keydown', (e) => {
+    if (e.ctrlKey && (e.key === 'u' || e.key === 'i' || e.key === 'c')) {
+        e.preventDefault();
+    }
+});
